ed7ad279560c3bffef948ae3dccf63cc41b0183e
k70118088-star <k70118088@gmail.com>
2026-02-09 12:21:06 +0530

    updated

--- src/Components/Forthpage/Eventbg.jsx
+++ src/Components/Forthpage/Eventbg.jsx
@@ -6,7 +6,7 @@ const Eventbg = () => {
   return (
     <div className='flex justify-center items-center'>
        
-        <div className="absolute inset-0 bg-[url(/assets/e17367a742c4a1c61951756ea6f76d0309c431af.jpg)] bg-no-repeat bg-center h-93 max-w-500 " />
+        <div className="absolute inset-0 bg-[url(/assets/e17367a742c4a1c61951756ea6f76d0309c431af.jpg)] bg-no-repeat bg-center h-93 " />
       <div className="absolute inset-0 bg-black/50 h-93  max-w-500" />
       <div className=' z-1 w-full max-w-7xl text-white mt-32 p-3'>
         <div className="flex items-start justify-flex-start gap-2 text-base font-normal ">
